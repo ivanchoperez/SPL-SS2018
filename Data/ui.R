567f4aafe7460a895d29e15a11576050ad673848
ivanchoperez <32841330+ivanchoperez@users.noreply.github.com>
2018-08-12 13:08:20 +0200

    Update ui.R

--- Data/ui.R
+++ Data/ui.R
@@ -2,30 +2,30 @@
 
 shinyUI(
   dashboardPage(
-  dashboardHeader(title = "SPL SS2018"),
-  dashboardSidebar(
-    selectInput("year", "Select Year",
-                choices = c("2012", "2015", "2018")),
-    selectInput("dictionary", "Select dictionary",
-                choices = c("NRC", "Minqing", "Sentimentr")),
-    selectInput("country", "Select country for wordcloud",
-                choices = c("austria", "belgium", "bulgaria", "cyprus",
+    dashboardHeader(title = "SPL SS2018"),
+    dashboardSidebar(
+      selectInput("year", "Select Year",
+                  choices = c("2012", "2015", "2018")),
+      selectInput("dictionary", "Select dictionary",
+                  choices = c("NRC", "Minqing", "Sentimentr")),
+      selectInput("country", "Select country for wordcloud",
+                  choices = c("austria", "belgium", "bulgaria", "cyprus",
                             "czech republic", "denmark", "estonia", "finland", "france",
                             "germany", "hungary", "ireland", "italy", "latvia", "lithuania",
                             "luxembourg", "malta", "netherlands", "poland", "portugal",
                             "romania", "slovakia", "slovenia", "spain", "sweden", "uk"))
-  ),
-  dashboardBody(
-    fluidRow(
-      box(title = "Sentiment Map",
-          status = "primary",
-          solidHeader = TRUE,
-          ggiraphOutput("map")),
-      box(title = "Country Wordcloud",
-          status = "primary",
-          solidHeader = TRUE, 
-          wordcloud2Output("wordcloud"))
+      ),
+    dashboardBody(
+      fluidRow(
+        box(title = "Sentiment Map",
+            status = "primary",
+            solidHeader = TRUE,
+            ggiraphOutput("map")),
+        box(title = "Country Wordcloud",
+            status = "primary",
+            solidHeader = TRUE, 
+            wordcloud2Output("wordcloud"))
+        )
+      )
     )
   )
-  )
-)
